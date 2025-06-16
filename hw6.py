1e34b52517da385516abe0af72ad99a7379639b2
demonchik666 <96588626+demonchik666@users.noreply.github.com>
2025-06-16 17:04:08 +0200

    Update hw6.py

--- hw6.py
+++ hw6.py
@@ -14,12 +14,8 @@ class Name(Field):
 class Phone(Field):
     #додавання валідації для номера телефону
     def __init__(self, value):
-        counter = 0
-        for symbol in value:
-            if symbol.isdigit():
-                counter += 1
-        if counter != 10:
-            raise ValueError("Phone number must contain exactly 10 digits.")
+        if not (value.isdigit() and len(value)==10):
+            raise ValueError(f"Phone number {value} must contain exactly 10 digits.")
         super().__init__(value)
 
 class Record:
@@ -33,20 +29,23 @@ class Record:
 
     #Зміна номера телефону з валідацією
     def edit_phone(self, old_phone, new_phone):
-        for i, phone in enumerate(self.phones):
-            if phone.value == old_phone:
-                try:
-                    self.phones[i] = Phone(new_phone)
-                except ValueError as e:
-                    print(e)
-                return
-        print("Phone number not found.")
+        self.find_phone(old_phone)  # Пошук старого номера
+        self.remove_phone(old_phone)  # Видаляємо старий номер
+        self.add_phone(new_phone)  # Додаємо новий номер
+        return None
 
     #Пошук номера телефону в записі
     def find_phone(self, phone):
         for given_phone in self.phones:
             if given_phone.value == phone:
-                return given_phone.value
+                return given_phone
+        return None
+    
+    def remove_phone(self, phone):
+        if self.find_phone(phone) is None:
+            raise ValueError(f"Phone number {phone} not found in record.")
+        else: self.phones.remove(self.find_phone(phone))
+        return None
 
     def __str__(self):
         return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
@@ -85,18 +84,25 @@ jane_record.add_phone("9876543210")
 book.add_record(jane_record)
 
 # Виведення всіх записів у книзі
-     
+print("Address Book:")
 print(book)
 
 # Знаходження та редагування телефону для John
 john = book.find("John")
 john.edit_phone("1234567890", "1112223333")
 
+print("After editing John's phone:")
 print(john)  # Виведення: Contact name: John, phones: 1112223333; 5555555555
 
 # Пошук конкретного телефону у записі John
+print("Finding phone number in John's record:")
 found_phone = john.find_phone("5555555555")
 print(f"{john.name}: {found_phone}")  # Виведення: John: 5555555555
 
 # Видалення запису Jane
 book.delete("Jane")
+
+# Видалення телефону з запису John
+john.remove_phone("5555555555")
+print("After removing Jane and one of John's phones:")
+print(book)  # Виведення: Contact name: John, phones: 1112223333
